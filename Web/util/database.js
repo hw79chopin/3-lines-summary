cf686a713720cd7388960cfc7618e6d9fe4bb6f0
HyunWoo Jung <chopin_liszt@naver.com>
2020-12-27 00:31:03 +0900

    Web ver1.0 deleted

--- Web/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('three_lines', 'root', '12341234', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
